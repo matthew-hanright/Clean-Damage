6f7a26c57de58b2c0bb2414d73718c0b31117163
Daniel Zale <workdanzale@gmail.com>
2020-02-01 11:49:10 -0500

    Made a working door

---
+++ Assets/Scripts/Dan_Test_Script/DoorLock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLock : MonoBehaviour
+{
+    private DoorOpen[] doors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doors = FindObjectsOfType<DoorOpen>();
+        if (doors != null)
+        {
+            foreach (DoorOpen door in doors)
+            {
+                Debug.Log(door);
+            }
+        }
+        else
+        {
+            Debug.Log("empty");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Dan_Test_Script/DoorOpen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public Animator animator;
+    bool broken = true;
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Player has to have the right material to fix the door
+        //The material will then be used and the door will always be fixed
+        if (collision.gameObject.tag == "interact" && broken)
+        {
+            Debug.Log("Fixed");
+            broken = false;
+        }
+        //This block controls the doors movement
+        else if (collision.gameObject.tag == "interact" && !animator.GetBool("open") && !broken)
+        {
+            //Debug.Log("Door is Open");
+            animator.SetBool("open", true);
+        }
+        else if(collision.gameObject.tag == "interact" && animator.GetBool("open") && !broken)
+        {
+            //Debug.Log("Door is Closed");
+            animator.SetBool("open", false);
+        }
+    }
+}
