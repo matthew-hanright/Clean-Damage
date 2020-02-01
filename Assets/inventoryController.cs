005feaa5ca31673c29ca3ca0235eaaeac6a55d7b
Thinkpad <grnpndrs4@gmail.com>
2020-02-01 18:10:25 -0500

    fixed inventory ui

---
+++ Assets/inventoryController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inventoryController : MonoBehaviour
+{
+    private PlayerController player;
+
+    public Text metal;
+    public Text plastic;
+    public Text rubber;
+    public Text electronics;
+    public Text glass;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    private void Update()
+    {
+        metal.text = player.materials[0] + "";
+        plastic.text = player.materials[1] + "";
+        rubber.text = player.materials[2] + "";
+        electronics.text = player.materials[3] + "";
+        glass.text = player.materials[4] + "";
+    }
+}
