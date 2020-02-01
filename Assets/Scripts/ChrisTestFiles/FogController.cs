9ac18eaccdeeae2aab08c0e4a521e81efb5d5d96
crav12345 <christopher.ravosa1@marist.edu>
2020-02-01 17:07:33 -0500

    Updated fogController and oxygenController. Electricity differences are now apparent. Working on starry background!

--- Assets/Scripts/ChrisTestFiles/FogController.cs
+++ Assets/Scripts/ChrisTestFiles/FogController.cs
@@ -11,18 +11,43 @@ public class FogController : MonoBehaviour
     // Boolean to determine if the player is in the room
     public bool playerHere = false;
 
+    public bool electricityRepaired = false;
+
     private void Update()
     {
-        // If the player is not here, turn the lights off
-        if (playerHere == false)
+        // If electricity is repaired...
+        if (electricityRepaired == true)
         {
-            if (my_Light.color.a < 0.9f)
-                DimLighting();
+            // ...and the player is not here...
+            if (playerHere == false)
+            {
+                // ...turn off the lights
+                if (my_Light.color.a < 1f)
+                    TurnOffLights();
+            } // if
+              // Otherwise, we want the lights all the way up,
+              // because we have electricity!
+            else if (my_Light.color.a > 0f)
+                TurnOnLights();
         } // if
-        // Otherwise, we want the lights on
+        // If electricity is not repaired...
         else
-            if (my_Light.color.a > 0f)
-                BrightenLighting();
+        {
+            // ...and the player is not here...
+            if (playerHere == false)
+            {
+                // ...turn off the lights again
+                if (my_Light.color.a < 1f)
+                    TurnOffLights();
+            } // if
+            // If the player is in the room with no electricity...
+            else
+            {
+                // ...we only want the lights adjusted a little
+                if (my_Light.color.a > 0.5f)
+                    BrightenLighting();
+            } // else
+        } // else
     } // Update
 
     // When something enters the room...
@@ -41,12 +66,19 @@ public class FogController : MonoBehaviour
             playerHere = false;
     } // OnTriggerExit2D
 
-    private void DimLighting()
+    private void TurnOffLights()
     {
         var tempColor = my_Light.color;
         tempColor.a += 0.1f;
         my_Light.color = tempColor;
     } // DimLighting
+
+    private void TurnOnLights()
+    {
+        var tempColor = my_Light.color;
+        tempColor.a -= 0.1f;
+        my_Light.color = tempColor;
+    } // TurnOnLights
 
     private void BrightenLighting()
     {
