07e31c21f7ac27fd8bb164f8748078aad9292418
Thinkpad <grnpndrs4@gmail.com>
2020-02-01 17:17:59 -0500

    added repair map

---
+++ Assets/mapController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mapController : MonoBehaviour
+{
+    public GameObject repairPrompt;
+    public Button electricity;
+    public Button oxygen;
+
+    private PlayerController player;
+
+    public GameObject activeRoom;
+
+    public bool isPrompting = false;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    public void prompt()
+    {
+        isPrompting = true;
+        repairPrompt.SetActive(true);
+        if (player.materials[3] < 1 || player.materials[4] < 1)
+        {
+            electricity.interactable = false;
+        }
+        else
+        {
+            electricity.interactable = true;
+        }
+
+        if (player.materials[1] < 1 || player.materials[2] < 1)
+        {
+            oxygen.interactable = false;
+        }
+        else
+        {
+            oxygen.interactable = true;
+        }
+    }
+
+    public void stopPrompt()
+    {
+        isPrompting = false;
+        repairPrompt.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        if(isPrompting)
+        {
+            stopPrompt();
+            activeRoom.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    public void fixElectric()
+    {
+        activeRoom.GetComponent<mapRoom>().fixRoomElectricity();
+        player.materials[3]--;
+        player.materials[4]--;
+    }
+
+    public void fixOxygen()
+    {
+        activeRoom.GetComponent<mapRoom>().fixRoomOxygen();
+        player.materials[1]--;
+        player.materials[2]--;
+    }
+}
---
+++ Assets/mapRoom.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapRoom : MonoBehaviour
+{
+    public GameObject room;
+
+    public Color hover = new Color(10, 100, 30, 255);
+    public Color click = new Color(30, 255, 50, 255);
+
+    private mapController map;
+
+    private void Start()
+    {
+        map = FindObjectOfType<mapController>();
+    }
+
+    private void OnMouseEnter()
+    {
+        if (!map.isPrompting)
+        {
+            GetComponent<SpriteRenderer>().color = hover;
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (!map.isPrompting)
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!map.isPrompting)
+        { 
+            GetComponent<SpriteRenderer>().color = click;
+            map.activeRoom = this.gameObject;
+            map.prompt();
+        }
+    }
+
+    public void fixRoomElectricity()
+    {
+
+    }
+
+    public void fixRoomOxygen()
+    {
+        room.GetComponent<OxygenController>().repaired = true;
+    }
+}
