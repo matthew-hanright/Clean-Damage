893caf42d123570e2454a3fd750d1b608ac7b201
crav12345 <christopher.ravosa1@marist.edu>
2020-02-02 14:55:45 -0500

    Adding ControlsOverlay, PausedUI, and Control Button Script. Going to link buttons once we add to main branch.

---
+++ Assets/Scripts/ChrisTestFiles/ControlsButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlsButton : MonoBehaviour
+{
+    public Button thisButton;
+    public GameObject controlsUI;
+
+    private void Start()
+    {
+        // ...add an onClick listener to the button
+        thisButton.onClick.AddListener(toggleControls);
+    } // Start
+
+    public void toggleControls()
+    {
+        if (controlsUI.active == true)
+        {
+            controlsUI.SetActive(false);
+        } // if
+        else
+            controlsUI.SetActive(true);
+    } // toggleControls
+} // ControlsButton
