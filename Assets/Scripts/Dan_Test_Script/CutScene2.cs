6ab76049be410c7533f4277dcf3c706b9822e3bb
Daniel Zale <workdanzale@gmail.com>
2020-02-02 11:07:46 -0500

    Added more sprites and shells for

---
+++ Assets/Scripts/Dan_Test_Script/CutScene2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScene2 : MonoBehaviour
+{
+    public Animator animator;
+    public bool stop = false;
+    public SpriteRenderer player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.enabled = false;
+        Dialouge();
+
+    }
+
+    private void Dialouge()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator.GetBool("twoDone") && stop)
+        {
+            stop = false;
+            Done();
+        }
+    }
+
+    private void Done()
+    {
+        player.enabled = true;
+    }
+}
