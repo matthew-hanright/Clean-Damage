276216c5b36d8f6f104fcd8d081e5037338f5b21
crav12345 <christopher.ravosa1@marist.edu>
2020-02-01 19:02:28 -0500

    Added manipulable timer script which we can add to a text component on a UI

---
+++ Assets/Scripts/ChrisTestFiles/TimerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class TimerController : MonoBehaviour
+{
+
+    public Text TimerText;
+    public float countdownTime = 60;
+    // Update is called once per frame
+    void Update()
+    {
+        countdownTime -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(countdownTime / 60F);
+        int seconds = Mathf.FloorToInt(countdownTime - minutes * 60);
+        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+        TimerText.text = niceTime;
+    } // Update
+} // TimerController
