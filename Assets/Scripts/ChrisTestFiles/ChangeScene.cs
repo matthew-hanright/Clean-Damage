925f13fade4e6aa32ac33c629c1b86681e2c86bf
crav12345 <christopher.ravosa1@marist.edu>
2020-02-02 12:40:28 -0500

    Commented code

--- Assets/Scripts/ChrisTestFiles/ChangeScene.cs
+++ Assets/Scripts/ChrisTestFiles/ChangeScene.cs
@@ -6,17 +6,25 @@ using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour
 {
-
+    // Attach this to the button which will change the scene. Here, we make
+    // a reference to the button which will be pressed
     public Button thisButton;
+
+    // This is a reference to the scene which we will go to. Literally just
+    // type the name of the preferred scene in the editor.
     public string goTo;
 
+    // When the scene starts...
     private void Start()
     {
+        // ...add an onClick listener to the button
         thisButton.onClick.AddListener(nextScene);
     } // Start
 
+    // This is the function called when the button is clicked
     public void nextScene()
     {
+        // It uses the Unity SceneManager to move to the desired scene
         SceneManager.LoadScene(goTo);
     } // nextScene
 } // ChangeScene
